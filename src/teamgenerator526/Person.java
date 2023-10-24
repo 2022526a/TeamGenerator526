effa4014f73f8e6c58b0357f16a543247ab6c6b9
2022526a <2022526@student.cct.ie>
2023-10-24 18:12:14 +0100

    Added Construtor.
    Added constructor  to the class person for the properties of person

--- src/teamgenerator526/Person.java
+++ src/teamgenerator526/Person.java
@@ -13,5 +13,12 @@ public class Person {
     String firstName;
     String lastName;
     String email;
+
+    public Person(int id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
     
 }
