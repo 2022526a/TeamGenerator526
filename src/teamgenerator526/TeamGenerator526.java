a13def3aaaf0cee1e0b43b0ecd46f6cb22f1f10f
2022526a <2022526@student.cct.ie>
2023-10-24 18:01:47 +0100

    Reading Data from file
    Reading Data from csv file .
    persons name
    persons id
    persons email

--- src/teamgenerator526/TeamGenerator526.java
+++ src/teamgenerator526/TeamGenerator526.java
@@ -6,6 +6,7 @@ package teamgenerator526;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 
 /**
  *
@@ -16,12 +17,21 @@ public class TeamGenerator526 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
         //adding csv file and reading it into memory
-        try (BufferedReader reader = new BufferedReader (new FileReader("MOCK_DATA.csv")))
-        {
-            String line;
+        try (BufferedReader reader = new BufferedReader (new FileReader("MOCK_DATA.csv"))){
+        String line;
+        while ((line = reader.readLine())!=null){
+            String [] parts=line.split(",");
+            int id = Integer.parseInt(parts[0]);
+            String firstName = parts[1];
+            String lastName = parts[2];
+            String email = parts[3];
+            Person person =new Person (id , firstName, lastName , email);
+            
+            
+        }
     }
     
     }
