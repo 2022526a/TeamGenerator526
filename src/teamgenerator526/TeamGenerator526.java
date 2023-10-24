8899518e5d3b690e61983da3f3440bc3c84c30be
2022526a <2022526@student.cct.ie>
2023-10-24 17:56:12 +0100

    Made two classes
    Classes has been made to implement the given questiona

--- src/teamgenerator526/TeamGenerator526.java
+++ src/teamgenerator526/TeamGenerator526.java
@@ -4,6 +4,9 @@
  */
 package teamgenerator526;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 /**
  *
  * @author 35389
@@ -15,6 +18,11 @@ public class TeamGenerator526 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //adding csv file and reading it into memory
+        try (BufferedReader reader = new BufferedReader (new FileReader("MOCK_DATA.csv")))
+        {
+            String line;
     }
     
+    }
 }
